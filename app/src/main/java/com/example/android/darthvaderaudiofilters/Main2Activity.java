fc07a175dd8322e2a7a037469afdb26e41308d6d
Nikita Chekalin <chekalin-kanztorg@yandex.ru>
2016-04-21 05:42:50 +0300

    Add ScrollView to activities

--- app/src/main/java/com/example/android/darthvaderaudiofilters/Main2Activity.java
+++ app/src/main/java/com/example/android/darthvaderaudiofilters/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.example.android.darthvaderaudiofilters;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Main2Activity extends Activity {
 
