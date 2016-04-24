d14b89cb164516bdde5b233377783fae949c3e6a
Nikita Chekalin <chekalin-kanztorg@yandex.ru>
2016-04-24 14:16:30 +0300

    Last changes

--- app/src/main/java/com/example/android/audiobydarthvader/Main2Activity.java
+++ app/src/main/java/com/example/android/audiobydarthvader/Main2Activity.java
@@ -10,14 +10,13 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
-import com.example.android.audiobydarthvader.R;
 
 import java.io.File;
 import java.io.IOException;
 
-public class Main2Activity extends Activity {
+public class Main2Activity extends Activity  {
 
     Button play,stop,record;
     private MediaRecorder myAudioRecorder;
@@ -44,25 +43,22 @@ public class Main2Activity extends Activity {
         myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
         myAudioRecorder.setOutputFile(outputFile);
 
+
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     myAudioRecorder.prepare();
                     myAudioRecorder.start();
-                }
-
-                catch (IllegalStateException e) {
+                } catch (IllegalStateException e) {
                     e.printStackTrace();
-                }
-
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
                 record.setEnabled(false);
                 stop.setEnabled(true);
-                stop.setTextColor(Color.parseColor("#F9A825"));
+                stop.setTextColor(Color.parseColor("#000000"));
                 record.setTextColor(Color.parseColor("#9E9E9E"));
 
                 Toast.makeText(getApplicationContext(), "Запись началась..", Toast.LENGTH_LONG).show();
@@ -74,11 +70,11 @@ public class Main2Activity extends Activity {
             public void onClick(View v) {
                 myAudioRecorder.stop();
                 myAudioRecorder.release();
-                myAudioRecorder  = null;
+                myAudioRecorder = null;
 
                 stop.setEnabled(false);
                 play.setEnabled(true);
-                play.setTextColor(Color.parseColor("#F9A825"));
+                play.setTextColor(Color.parseColor("#000000"));
                 stop.setTextColor(Color.parseColor("#9E9E9E"));
 
                 Toast.makeText(getApplicationContext(), "Успешно записано!", Toast.LENGTH_LONG).show();
@@ -87,22 +83,18 @@ public class Main2Activity extends Activity {
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) throws IllegalArgumentException,SecurityException,IllegalStateException {
+            public void onClick(View v) throws IllegalArgumentException, SecurityException, IllegalStateException {
                 MediaPlayer m = new MediaPlayer();
 
                 try {
                     m.setDataSource(outputFile);
-                }
-
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
                 try {
                     m.prepare();
-                }
-
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
@@ -110,10 +102,15 @@ public class Main2Activity extends Activity {
                 Toast.makeText(getApplicationContext(), "Воспроизведение аудио", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
     public void send(View view) {
+
+        EditText subj = (EditText)findViewById(R.id.subject_mail);
+        EditText text = (EditText)findViewById(R.id.text_mail);
+
+        String subj_text = subj.getText().toString();
+        String mail_text = text.getText().toString();
 
         File fileIn = new File(outputFile);
 
@@ -121,7 +118,8 @@ public class Main2Activity extends Activity {
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
         emailIntent.setType("Audio/mp3");
         emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Смотри, что я записал!");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subj_text);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, mail_text);
         emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
 
         if (emailIntent.resolveActivity(getPackageManager()) != null) {
@@ -129,6 +127,10 @@ public class Main2Activity extends Activity {
             Toast.makeText(this, "Отправляем...", Toast.LENGTH_SHORT).show();
         }
     }
+
+//    public void doDarth(View view){
+//
+//    }
 
     public void back2Act(View view) {
         Intent intent = new Intent(Main2Activity.this, MainActivity.class);
--- app/src/main/java/com/example/android/audiobydarthvader/MainActivity.java
+++ app/src/main/java/com/example/android/audiobydarthvader/MainActivity.java
@@ -1,29 +1,35 @@
 package com.example.android.audiobydarthvader;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.android.audiobydarthvader.R;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView dart=(TextView)findViewById(R.id.luk_maker);
-
     }
 
-
-
+    public void exit(View view) {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
+    }
 
     public void GoTo2Act(View view) {
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(intent);
     }
+
 }
